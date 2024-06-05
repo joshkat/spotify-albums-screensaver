dd4e91665b89d63297683d6497cb63840134a5d0
joshkat <joshua02k@gmail.com>
2024-06-05 00:03:42 -0400

    Added Link buttons on / and /403 - Use Link for SSR - Removed old components folder

--- app/403/page.jsx
+++ app/403/page.jsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 export default function Error() {
   return (
-    <main className="flex justify-center items-center min-h-screen">
+    <main className="flex justify-center items-center min-h-screen flex-col gap-8">
       <div className="flex">
         <h1 className="text-3xl">403</h1>
         <div className="divider divider-error divider-horizontal"></div>
         <p className="text-xl flex items-center">Forbidden</p>
       </div>
+      <Link href="/" className="btn">
+        Return Home
+      </Link>
     </main>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,19 @@
-import LoginButton from "./components/LoginButton";
+import Link from "next/link";
 
 export default function Home() {
+  const scope = "user-library-read playlist-read-private";
+  const redirect_uri = process.env.REDIRECT_URI;
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const redirect_link = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}&show_dialog=true`;
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-between p-24"
       suppressHydrationWarning
     >
       login to get started
-      <LoginButton />
+      <Link href={redirect_link} className="btn">
+        Login to Spotify
+      </Link>
     </main>
   );
 }
