7e30a312f3285d98d2fe4555599304e724846f93
joshkat <joshua02k@gmail.com>
2024-08-06 10:55:45 -0400

    Updated Screensaver page to be functional with previously used ImageContainer - Added new 400 error page to handle errors - Used React Suspense to have pages have a loading instead of being stuck

---
+++ app/400/page.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Error({ searchParams }) {
+  console.log(searchParams);
+  return (
+    <main className="flex justify-center items-center min-h-screen flex-col gap-8">
+      <div className="flex">
+        <h1 className="text-3xl">400</h1>
+        <div className="divider divider-error divider-horizontal"></div>
+        <p className="text-xl flex items-center">Bad Request</p>
+      </div>
+      {searchParams.error === undefined ? (
+        ""
+      ) : (
+        <p className="text-center">
+          Error: &quot;{searchParams.error}&quot; <br /> And that&apos;s all we
+          know
+        </p>
+      )}
+      <Link href="/" className="btn">
+        Return Home
+      </Link>
+    </main>
+  );
+}
--- app/success/screensaver/page.jsx
+++ app/success/screensaver/page.jsx
@@ -1,3 +1,66 @@
-export default function Screensaver() {
-  return <div>hello!</div>;
+import { redirect } from "next/navigation";
+import ImageContainer from "./ImageContainer";
+import { Suspense } from "react";
+
+export default async function Screensaver({ searchParams }) {
+  const id = searchParams.id;
+  const token = searchParams.token;
+  if (id === "" || id === undefined || token === "" || token === undefined) {
+    redirect("/403");
+  }
+
+  const playlistURL =
+    id === "liked"
+      ? `https://api.spotify.com/v1/me/tracks?limit=50`
+      : `https://api.spotify.com/v1/playlists/${id}/tracks?limit=50`;
+  const albumImageURLs = await getImageURLs(new Set(), playlistURL, token);
+  if (albumImageURLs.length < 3) {
+    redirect(
+      `/400?error=Your playlist doesn't contain enough songs to generate art`
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-12 break-words">
+      <Suspense fallback={<p>Loading...</p>}>
+        <ImageContainer imageArray={albumImageURLs} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function getImageURLs(albumSet, url, token) {
+  if (albumSet.size >= 80 || url === null) {
+    // base case when we hit desired num of URLs or when URL is ""
+    return [...albumSet];
+  }
+
+  const req = fetch(url, {
+    cache: "no-cache",
+    method: "GET",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  let res = await req;
+  if (res.status != 200) {
+    // something went down, redirect out of here
+    console.log(await res.text(), res.status, token);
+    redirect("/403");
+  }
+
+  const json = await res.json();
+  let newSet = new Set(albumSet);
+
+  json.items.map((itemsObj) => {
+    const setObj = {
+      albumURL: itemsObj?.track?.album?.external_urls?.spotify,
+      songCover: itemsObj?.track?.album?.images[0]?.url,
+    };
+    newSet.add(JSON.stringify(setObj));
+  });
+  // recurse!
+  return getImageURLs(newSet, json.next, token);
 }
